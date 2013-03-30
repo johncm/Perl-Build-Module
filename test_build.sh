44246ef4306d44890516a3bb3287abccbabaa3e7
John Chambers-Malewig <johncm@rcn.com>
2013-03-29 21:35:11 -0500

    Update test_build.sh
    Added subdirectory cpanlib to PERL_CPANM_HOME if it is not populated and is then set to ${WORKSPACE}
    Run prove only if TAP::Formatter::JUnit Module is successfully installed
    Define destination of prove output to be the ${WORKSPACE}
    Build testcover only if the Devel::Cover Module is successfully installed

--- test_build.sh
+++ test_build.sh
@@ -2,7 +2,7 @@
 
 source ../Build-Module/build.sh
 
-export PERL_CPANM_HOME=${PERL_CPANM_HOME:-${WORKSPACE}}
+export PERL_CPANM_HOME=${PERL_CPANM_HOME:-${WORKSPACE}/cpanlib}
 OPT=--local-lib=${PERL_CPANM_HOME}
 export PERL_CPANM_OPT=${PERL_CPANM_OPT:-${OPT}}
 
@@ -10,12 +10,12 @@ export PATH=${PERL_CPANM_HOME}/bin:${PATH}
 export PERL5LIB=${PERL5LIB:-${PERL_CPANM_HOME}/lib/perl5}
 
 if [ -x `which cpanm` ]; then
-   cpanm -v --notest TAP::Formatter::JUnit
    # Make sure to setup the "Publish JUnit test result report" with output location
+   cpanm -v --notest TAP::Formatter::JUnit &&
    prove -I${PERL5LIB} --blib --timer --formatter=TAP::Formatter::JUnit > \
-         ../prove_output.xml &&
-   cpanm -v PJCJ/Devel-Cover-0.73.tar.gz
-   # Make sure to setup the "Publish HTML reports" to cover_db
+         ${WORKSPACE}/prove_output.xml &&
+   cpanm -v PJCJ/Devel-Cover-0.73.tar.gz &&
    ./Build testcover
-   ./Build html
+   # Make sure to setup the "Publish HTML reports" to cover_db
 fi
+./Build html
